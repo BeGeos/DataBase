155aa62fed1611ae1f7bf695cb37c1e38fe83d95
BeGeos <montepeloso.m@gmail.com>
2020-10-15 18:46:23 +0100

    Added join seach

--- table.py
+++ table.py
@@ -23,40 +23,6 @@ class DataBase:
             cache_tables[table[0]] = cache_columns
         return cache_tables
 
-    def many_to_many(self, name, args: tuple or list, d_type: tuple or list):
-        """ To create relations many to many. It is important that the data are iterables and more
-        than 1. In addition, it is also essential to specify the column and the table it is referring to
-        in the args argument, in this way: Table.column. It is also recommended to rename the id column
-        in the main table not create overlaps or redundancies. """
-        if len(args) <= 1:
-            raise TypeError
-        elif len(args) != len(d_type):
-            raise TypeError
-        fk_string = ''
-        column_string = ''
-        d_type_string = ''
-        # It creates the first part of the SQL query to create the column and their data type and
-        # the second part of the query which match the foreign keys and the tables, all together
-        for each in zip(args, d_type):
-            parsed = each[0].split('.')
-            table = parsed[0]
-            column = parsed[1]
-            column_string += column + ","
-            d_type_string += column + " " + each[1] + " NOT NULL,"
-            fk_string += "FOREIGN KEY (" + column + ") REFERENCES " + table + "(" + column + "),"
-        unique_string = fk_string + "UNIQUE (" + column_string[:-1] + ")"
-        cat_string = "CREATE TABLE " + name + "(" + d_type_string + unique_string + ")"
-        if DataBase.string_sql:
-            print(cat_string)
-        else:
-            cur = self.cur
-            try:
-                cur.execute(cat_string)
-                self.conn.commit()
-                print("'{}' created successfully".format(name))
-            except sqlite3.OperationalError as op:
-                print(op)
-
     @staticmethod
     def print_sql(p=False):
         if p:
@@ -93,7 +59,7 @@ class Table(DataBase):
             for key in kwargs:
                 condition_query += key + ' ' + kwargs[key] + ', '
             cat_string = ("CREATE TABLE " + self.name + "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-                                                        + condition_query[:-2] + ")")
+                          + condition_query[:-2] + ")")
         else:
             cat_string = ("CREATE TABLE " + self.name + "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)")
         # print(cat_string)
@@ -132,9 +98,11 @@ class Table(DataBase):
             print(op)
 
     def insert_column(self, column, d_type=''):
-        """ Create columns in the table object """
+        """ Create columns in the table object one at a time """
 
         cur = self.cur
+        column.rstrip()
+        column = column.replace(' ', '_')
         cat_string = ('ALTER TABLE ' + self.name + ' ADD ' + column + ' ' + d_type.upper())
         print(cat_string)
         try:
@@ -146,7 +114,9 @@ class Table(DataBase):
 
     def insert_foreign_key(self, column_from, column_to, d_type):
         """ It allows to create an individual column with the attribute foreign key.
-        It is essential to refer the table name in the column to as argument as Table_name.column_fk """
+        It is essential to refer the table name in the column_to as argument as Table_name.column_fk.
+         It is also a best practice to have the names of both foreign keys identical to simply
+         the search. See join_search"""
 
         cur = self.cur
         column_to = column_to.split('.')
@@ -160,7 +130,42 @@ class Table(DataBase):
         except sqlite3.OperationalError as op:
             print(op)
 
-    def drop_column(self, column):  # to be updated
+    def many_to_many(self, keys: tuple or list, d_type: tuple or list):  # tbu as dictionary
+        """ To create relations many to many. It is important that the data are iterables and more
+        than 1. In addition, it is also essential to specify the column and the table it is referring to
+        in the args argument, in this way: Table.column. It is also recommended to rename the id column
+        in the main table not create overlaps or redundancies. """
+
+        if len(keys) <= 1:
+            raise TypeError
+        elif len(keys) != len(d_type):
+            raise TypeError
+        fk_string = ''
+        column_string = ''
+        d_type_string = ''
+        # It creates the first part of the SQL query to create the column and their data type and
+        # the second part of the query which match the foreign keys and the tables, all together
+        for each in zip(keys, d_type):
+            parsed = each[0].split('.')
+            table = parsed[0]
+            column = parsed[1]
+            column_string += column + ","
+            d_type_string += column + " " + each[1] + " NOT NULL,"
+            fk_string += "FOREIGN KEY (" + column + ") REFERENCES " + table + "(" + column + "),"
+        unique_string = fk_string + "UNIQUE (" + column_string[:-1] + ")"
+        cat_string = "CREATE TABLE " + self.name + "(" + d_type_string + unique_string + ")"
+        if DataBase.string_sql:
+            print(cat_string)
+        else:
+            cur = self.cur
+            try:
+                cur.execute(cat_string)
+                self.conn.commit()
+                print("'{}' created successfully".format(self.name))
+            except sqlite3.OperationalError as op:
+                print(op)
+
+    def delete_column(self, column):  # to be updated
         """ Drop a column or a series of columns """
 
         cur = self.cur
@@ -237,7 +242,7 @@ class Table(DataBase):
             quit()
         return condition_query
 
-    def fetch_all(self, column=(), _and=False, _or=False, **kwargs):  # SELECT column FROM Table WHERE ""
+    def fetch(self, num=0, column=(), _and=False, _or=False, **kwargs):  # SELECT column FROM Table WHERE ""
         """ Write a query in the database """
 
         cur = self.cur
@@ -252,7 +257,10 @@ class Table(DataBase):
 
         # It writes the conditional part of the query if there is 0, one or more and it allows AND and OR statements
         if len(kwargs) == 0:
-            cat_string = ('SELECT ' + column_string + ' FROM ' + self.name)
+            if num != 0:
+                cat_string = 'SELECT ' + column_string + ' FROM ' + self.name + ' LIMIT ' + str(num)
+            else:
+                cat_string = ('SELECT ' + column_string + ' FROM ' + self.name)
             try:
                 cur.execute(cat_string)
                 result = cur.fetchall()
@@ -265,6 +273,8 @@ class Table(DataBase):
         else:
             condition_query = Table.if_type(kwargs)
         cat_string = ("SELECT " + column_string + " FROM " + self.name + " WHERE " + condition_query)
+        if num != 0:
+            cat_string = cat_string + " LIMIT " + str(num)
         try:
             cur.execute(cat_string)
             result = cur.fetchall()
@@ -272,23 +282,49 @@ class Table(DataBase):
         except sqlite3.OperationalError as op:
             print(op)
 
-    def fetch_one(self, column=(), _and=False, _or=False, **kwargs):
-        result = Table.fetch_all(self, column, _and, _or, **kwargs)
-        if len(result) != 0:
-            return result[0]
-        print('No records found')
+    def join_search(self, table, key, column: tuple or list, num=0, _and=False, _or=False, **kwargs):
+        """ Implementing join search in SQL database. Make sure that the key name is identical
+         in the table you want to join """
 
-    def fetch_head(self, num=5, column=(), _and=False, _or=False, **kwargs):
-        result = Table.fetch_all(self, column, _and, _or, **kwargs)
-        if len(result) != 0:
-            return result[:num]
-        print('No records found')
+        cur = self.cur
+        # It writes the column part of the query
+        column_string = ''
+        if len(column) == 0:
+            column_string = '*'
+        else:
+            for c in column:
+                column_string += c + ", "
+            column_string = column_string[:-2]
+        # It write the conditional part of the query WHERE = ...
+        if len(kwargs) == 0:
+            if num != 0:
+                cat_string = "SELECT " + column_string + " FROM " + self.name + " JOIN " + table + \
+                             " USING (" + key + ") LIMIT " + str(num)
+            else:
+                cat_string = "SELECT " + column_string + " FROM " + self.name + " JOIN " + table + \
+                             " USING (" + key + ")"
+            try:
+                cur.execute(cat_string)
+                result = cur.fetchall()
+                return result
+            except sqlite3.OperationalError as op:
+                print(op)
+                quit()
+        elif len(kwargs) > 1:
+            condition_query = Table.and_or_query(kwargs, _and, _or)
+        else:
+            condition_query = Table.if_type(kwargs)
 
-    def fetch_tail(self, num=5, column=(), _and=False, _or=False, **kwargs):
-        result = Table.fetch_all(self, column, _and, _or, **kwargs)
-        if len(result) != 0:
-            return result[-num:]
-        print('No records found')
+        cat_string = "SELECT " + column_string + " FROM " + self.name + " JOIN " + table + \
+                     " USING (" + key + ") WHERE " + condition_query
+        if num != 0:
+            cat_string = cat_string + " LIMIT " + str(num)
+        try:
+            cur.execute(cat_string)
+            result = cur.fetchall()
+            return result
+        except sqlite3.OperationalError as op:
+            print(op)
 
     def update_data(self, column, new_data, _and=False, _or=False, **kwargs):  # to be updated AND and OR together
         """ It updates data into rows under a specific condition i.g: column=value """
